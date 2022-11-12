bfa230b1fcbf3c530d29cbc94d87f0857e3ef705
Desturo <47252476+Desturo@users.noreply.github.com>
2022-11-12 16:10:13 +0100

    Bomberman v1

--- Übung_5_1/main.cpp
+++ Übung_5_1/main.cpp
@@ -10,22 +10,33 @@ using std::endl;
 using std::chrono::milliseconds;
 using std::setw;
 
-void drawField(unsigned int playerPos[2], unsigned int enemyPos[2], unsigned int bombs[5][3], bool gameState, unsigned long long &gameTime);
+void drawField(unsigned int playerPos[2], unsigned int enemyPos[2], unsigned int bombs[5][3], bool gameState, unsigned int explosion[7][7][2], unsigned int points);
 
 void movePlayer(unsigned int playerPos[2], unsigned char &direction);
 void moveEnemy(unsigned int enemyPos[2], unsigned char &direction);
 
 void placeBomb(unsigned int playerPos[2], unsigned int bombs[5][3], unsigned char *bombCount, unsigned long long &gameTime);
+void resetExplosion(unsigned int explosion[7][7][2]);
 
 int main() {
 	bool running = true;
 	unsigned char bombCount = 0;
 	unsigned long long gameTime = 0;
+	unsigned int points = 0;
 
 	unsigned int playerPos[] = { 15, 10 };
 	unsigned int enemyPos[] = { 12, 12 };
 	//the third value is their time code, for knowing when they have to explode
 	unsigned int bombs[5][3] = { {0,0,0},{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} };
+	unsigned int explosion[7][7][2] = {
+		{{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}},
+		{{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}},
+		{{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}},
+		{{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}},
+		{{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}},
+		{{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}},
+		{{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}}
+	};
 
 	/*
 		0 -> UP [W]
@@ -71,20 +82,43 @@ int main() {
 					break;
 			}
 		}
+
+		resetExplosion(explosion);
 		//check if any bomb exploded
 		for (size_t i = 0; i < 5; i++)
 		{
-			if (gameTime - bombs[i][2] >= 6) {
+			if (gameTime - bombs[i][2] >= 6 && bombs[i][2] != 0) {
+
+				for(size_t j = 0; j < 7; j++) {
+					for (size_t k = 0; k < 7; k++) {
+						explosion[j][k][0] = bombs[i][0] - (3 - k);
+						explosion[j][k][1] = bombs[i][1] - (3 - j);
+					}
+				}
+
 				bombs[i][0] = 0;
 				bombs[i][1] = 0;
 				bombs[i][2] = 0;
+
+				//looping over every flield exploded and check if the player is on one of the fields.
+				for (size_t exploX = 0; exploX < 7; exploX++) {
+					for (size_t exploY = 0; exploY < 7; exploY++) {
+						if (explosion[exploX][exploY][0] == playerPos[0] && explosion[exploX][exploY][1] == playerPos[1]) running = false;
+						if (explosion[exploX][exploY][0] == enemyPos[0] && explosion[exploX][exploY][1] == enemyPos[1]) points++;
+					}
+				}
 			}
 		}
 
+		
+
 		movePlayer(playerPos, direction);
 		moveEnemy(enemyPos, enemyDirection);
+
+		//check if player and enemy collide
+		if (playerPos[0] == enemyPos[0] && playerPos[1] == enemyPos[1]) running = false;
 		//Draw Field
-		drawField(playerPos, enemyPos, bombs, running, gameTime);
+		drawField(playerPos, enemyPos, bombs, running, explosion, points);
 
 		gameTime++;
 	}
@@ -93,10 +127,11 @@ int main() {
 	return 200;
 }
 
-void drawField(unsigned int playerPos[2], unsigned int enemyPos[2], unsigned int bombs[5][3], bool gameState, unsigned long long &gameTime) {
+void drawField(unsigned int playerPos[2], unsigned int enemyPos[2], unsigned int bombs[5][3], bool gameState, unsigned int explosion[7][7][2], unsigned int points) {
 	system("cls");
 	cout << setw(31) << std::setfill('-') << "-" << endl;
 	bool bomb = false;
+	bool explo = false;
 
 	for (size_t i = 1; i <= 20; i++)
 	{
@@ -112,9 +147,21 @@ void drawField(unsigned int playerPos[2], unsigned int enemyPos[2], unsigned int
 				}
 			}
 
+			for (size_t l = 0; l < 7; l++)
+			{
+				for (size_t m = 0; m < 7; m++)
+				{
+					if (explosion[l][m][0] == j && explosion[l][m][1] == i) explo = true;
+				}
+			}
+
 			//Order of IFs decides what get's rendered if two entities overlap
 			if (playerPos[0] == j && playerPos[1] == i) {
 				cout << "P";
+			}
+			else if (explo)
+			{
+				cout << "\260";
 			}
 			else if (enemyPos[0] == j && enemyPos[1] == i) {
 				cout << "E";
@@ -128,12 +175,13 @@ void drawField(unsigned int playerPos[2], unsigned int enemyPos[2], unsigned int
 				cout << " ";
 			}
 			bomb = false;
+			explo = false;
 		}
 			
 		cout << "|" << endl;
 	}
 	cout << setw(31) << std::setfill('-') << "-" << endl;
-	cout << (gameState ? "Playing..." : "Game Over!") << " Tick: " << gameTime << endl;
+	cout << (gameState ? "Playing..." : "Game Over!") << " Points: " << points << endl;
 }
 
 void movePlayer(unsigned int playerPos[2], unsigned char &direction) {
@@ -206,6 +254,16 @@ void placeBomb(unsigned int playerPos[2], unsigned int bombs[5][3], unsigned cha
 			bombs[i][1] = playerPos[1];
 			bombs[i][2] = gameTime;
 			break;
+		}
+	}
+}
+
+void resetExplosion(unsigned int explosion[7][7][2]) {
+
+	for(size_t i = 0; i < 7; i++) {
+		for (size_t j = 0; j < 7; j++) {
+			explosion[i][j][0] = 0;
+			explosion[i][j][1] = 0;
 		}
 	}
 }
