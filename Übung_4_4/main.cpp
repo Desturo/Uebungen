768ae21eb85aa0d04536f4a45fd9bc7b81efce14
Desturo <47252476+Desturo@users.noreply.github.com>
2022-11-12 02:05:13 +0100

    Uebung 4.4 v1

--- Übung_4_4/main.cpp
+++ Übung_4_4/main.cpp
@@ -6,20 +6,27 @@
 
 float cbvTranslate(float ang);
 void cbrTranslate(float& ang);
+void cbpTranslate(float* angAdr);
 
 int main() {
 
-	float ang1 = 1;
+	float ang1 = 22;
 
 	std::cout << std::setprecision(8) << "Call By Value: " << ang1 << "\370 = ";
 	ang1 = cbvTranslate(ang1);
 	std::cout << ang1 << " RAD" << std::endl;
 
-	float ang2 = 1;
+	float ang2 = 22;
 
 	std::cout << "Call By Reference: " << ang2 << "\370 = ";
 	cbrTranslate(ang2);
 	std::cout << ang2 << " RAD" << std::endl;
+
+	float ang3 = 22;
+
+	std::cout << "Call By Pointer: " << ang3 << "\370 = ";
+	cbpTranslate(&ang3);
+	std::cout << ang3 << " RAD" << std::endl;
 
 	return 200;
 }
@@ -30,4 +37,8 @@ float cbvTranslate(float ang) {
 
 void cbrTranslate(float &ang) {
 	ang = ang * (M_PI / 180);
+}
+
+void cbpTranslate(float *angAdr) {
+	*angAdr = *angAdr * (M_PI / 180);
 }
